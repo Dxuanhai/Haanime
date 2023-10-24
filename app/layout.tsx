2a69457568ebdab495f2ef5d44e1cd0eeaf101f1
xuanhaibaka <dauhai2005@gmail.com>
2023-10-24 16:51:43 +0700

    inital/ messages

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { cn } from "@/lib/utils";
 import { Toaster } from "@/components/ui/toaster";
 import { ModalProvider } from "@/components/providers/modal-provider";
 import { SocketProvider } from "@/components/providers/socket-provider";
+import { QueryProvider } from "@/components/providers/query-provider";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -39,7 +40,7 @@ export default function RootLayout({
           >
             <SocketProvider>
               <ModalProvider />
-              {children}
+              <QueryProvider>{children}</QueryProvider>
             </SocketProvider>
             <Toaster />
           </ThemeProvider>
