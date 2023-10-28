9476d2a590c656e2b0b1a239f75e719a301f7564
xuanhaibaka <dauhai2005@gmail.com>
2023-10-28 15:58:06 +0700

    .....

--- middleware.ts
+++ middleware.ts
@@ -6,6 +6,7 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   publicRoutes: ["/api/uploadthing", "/"],
+  ignoredRoutes: ["/((?!api|trpc))(_next|.+..+)(.*)", "/"],
 });
 
 export const config = {
